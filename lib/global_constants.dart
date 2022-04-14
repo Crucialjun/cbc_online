2c948c097719a475fa748aa50e55edbc9111af52
Crucial Jun <nich.otieno@gmail.com>
2022-04-14 15:01:18 +0300

    add onboarding json

--- lib/global_constants.dart
+++ lib/global_constants.dart
@@ -2,3 +2,9 @@ String appName = "CBC Online";
 
 String lorem =
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+//Onboarding data
+String onboardingTableName = "Onboarding";
+String onboardingTitle = "Title";
+String assetName = "assetname";
+String text = "Text";
